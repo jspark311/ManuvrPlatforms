1fa346536f6973ad497c3d07c053e4a9affde9ab
jspark311 <josh.lindsay@gmail.com>
2021-06-25 23:57:26 -0600

    Fixing some problems in Teensy4 I2CAdapter.

--- ManuvrArduino/src/Teensy4/I2CAdapter.cpp
+++ ManuvrArduino/src/Teensy4/I2CAdapter.cpp
@@ -32,12 +32,14 @@ int8_t I2CAdapter::bus_init() {
   switch (ADAPTER_NUM) {
     case 0:
       if ((18 == _bus_opts.sda_pin) && (19 == _bus_opts.scl_pin)) {
+        master0.end();
         master0.begin(_bus_opts.freq);
         busOnline(true);
       }
       break;
     case 1:
       if ((17 == _bus_opts.sda_pin) && (16 == _bus_opts.scl_pin)) {
+        master1.end();
         master1.begin(_bus_opts.freq);
         busOnline(true);
       }
@@ -52,10 +54,8 @@ int8_t I2CAdapter::bus_init() {
 int8_t I2CAdapter::bus_deinit() {
   busOnline(false);
   switch (ADAPTER_NUM) {
-    case 0:
-      break;
-    case 1:
-      break;
+    case 0:   master0.end();   break;
+    case 1:   master1.end();   break;
     default:
       return -1;
   }
@@ -86,6 +86,9 @@ int8_t I2CAdapter::generateStop() {
 * _|_ /  \_/ o   \_| (_) |_)
 *******************************************************************************/
 
+uint8_t _dead_buf[2][256] = {0, };
+
+
 XferFault I2CBusOp::begin() {
   I2CMaster* adptr = nullptr;
   uint8_t ord = 0;
@@ -94,33 +97,41 @@ XferFault I2CBusOp::begin() {
     case 1:    adptr = &master1;             break;
     default:   abort(XferFault::BAD_PARAM);  return getFault();
   }
+  if (!adptr->finished()) {
+    return XferFault::BUS_BUSY;
+  }
   set_state(XferState::INITIATE);  // Indicate that we now have bus control.
 
   switch (get_opcode()) {
     case BusOpcode::TX_CMD:
+      //set_state(XferState::ADDR);
       set_state(XferState::TX_WAIT);
-      adptr->write_async(dev_addr, _buf, 0, true);
+      adptr->write_async(dev_addr, &_dead_buf[device->adapterNumber()][0], 0, 1);
       break;
 
     case BusOpcode::TX:
       if (need_to_send_subaddr()) {
-        set_state(XferState::ADDR);
-        adptr->write_async(dev_addr, (uint8_t*) &sub_addr, 1, false);
+        set_state(XferState::TX_WAIT);
+        _dead_buf[device->adapterNumber()][0] = (uint8_t) sub_addr;
+        for (uint x = 0; x < strict_min(255, _buf_len); x++) {
+          _dead_buf[device->adapterNumber()][x+1] = _buf[x];
+        }
+        adptr->write_async(dev_addr, &_dead_buf[device->adapterNumber()][0], (_buf_len+1), 1);
       }
       else {
         set_state(XferState::TX_WAIT);
-        adptr->write_async(dev_addr, _buf, _buf_len, true);
+        adptr->write_async(dev_addr, _buf, _buf_len, 1);
       }
       break;
 
     case BusOpcode::RX:
       if (need_to_send_subaddr()) {
         set_state(XferState::ADDR);
-        adptr->write_async(dev_addr, (uint8_t*) &sub_addr, 1, false);
+        adptr->write_async(dev_addr, (uint8_t*) &sub_addr, 1, 0);
       }
       else {
         set_state(XferState::RX_WAIT);
-        adptr->read_async(dev_addr, _buf, _buf_len, true);
+        adptr->read_async(dev_addr, _buf, _buf_len, 1);
       }
       break;
 
@@ -133,85 +144,118 @@ XferFault I2CBusOp::begin() {
 
 
 
-
 /*
-* FreeRTOS doesn't have a concept of interrupt, but we might call this
-*   from an I/O thread.
 */
 XferFault I2CBusOp::advance(uint32_t status_reg) {
-  XferFault ret = XferFault::NONE;
   I2CMaster* adptr = nullptr;
   switch (device->adapterNumber()) {
     case 0:    adptr = &master0;             break;
     case 1:    adptr = &master1;             break;
     default:   abort(XferFault::BAD_PARAM);  return getFault();
   }
-  switch (get_state()) {
-    case XferState::UNDEF:
-    case XferState::IDLE:
-    case XferState::QUEUED:
-    case XferState::INITIATE:
-      break;
-    case XferState::ADDR:
+
+  switch (adptr->error()) {
+    case I2CError::ok:
       if (adptr->finished()) {
-        switch (get_opcode()) {
-          case BusOpcode::RX:
-            if (adptr->get_bytes_transferred() == 1) {
-              set_state(XferState::RX_WAIT);
-              adptr->read_async(dev_addr, _buf, _buf_len, true);
-            }
-            else {
-              abort(XferFault::DEV_NOT_FOUND);
+        switch (get_state()) {
+          case XferState::UNDEF:
+          case XferState::IDLE:
+          case XferState::QUEUED:
+          case XferState::INITIATE:
+            break;
+          case XferState::ADDR:
+            switch (get_opcode()) {
+              case BusOpcode::RX:
+                if (adptr->get_bytes_transferred() == 1) {
+                  set_state(XferState::RX_WAIT);
+                  adptr->read_async(dev_addr, _buf, _buf_len, 1);
+                }
+                else {
+                  abort(XferFault::DEV_NOT_FOUND);
+                }
+                break;
+              case BusOpcode::TX:
+                if (adptr->get_bytes_transferred() == 1) {
+                  set_state(XferState::TX_WAIT);
+                  adptr->write_async(dev_addr, _buf, _buf_len, 1);
+                }
+                else {
+                  abort(XferFault::DEV_NOT_FOUND);
+                }
+                break;
+              case BusOpcode::TX_CMD:
+                set_state(XferState::TX_WAIT);
+                adptr->write_async(dev_addr, &_dead_buf[device->adapterNumber()][0], 0, 1);
+                break;
+              default:
+                abort(XferFault::ILLEGAL_STATE);
+                break;
             }
             break;
-          case BusOpcode::TX:
-            if (adptr->get_bytes_transferred() == 1) {
-              set_state(XferState::TX_WAIT);
-              adptr->write_async(dev_addr, _buf, _buf_len, true);
-            }
-            else {
-              abort(XferFault::DEV_NOT_FOUND);
+          case XferState::TX_WAIT:
+          case XferState::RX_WAIT:
+            switch (get_opcode()) {
+              case BusOpcode::RX:
+                if (adptr->get_bytes_transferred() == _buf_len) {
+                  markComplete();
+                }
+                else {
+                  abort(XferFault::BUS_FAULT);
+                }
+                break;
+              case BusOpcode::TX:
+                if (adptr->get_bytes_transferred() == (_buf_len+1)) {
+                  markComplete();
+                }
+                else {
+                  abort(XferFault::BUS_FAULT);
+                }
+                break;
+              case BusOpcode::TX_CMD:
+                markComplete();
+                break;
+              default:
+                abort(XferFault::ILLEGAL_STATE);
+                break;
             }
             break;
+          case XferState::STOP:
+            markComplete();
+            break;
+          case XferState::COMPLETE:
+          case XferState::FAULT:
           default:
-            abort(XferFault::ILLEGAL_STATE);
             break;
         }
       }
       break;
-    case XferState::TX_WAIT:
-    case XferState::RX_WAIT:
-      if (adptr->finished()) {
-        switch (get_opcode()) {
-          case BusOpcode::RX:
-          case BusOpcode::TX:
-            if (adptr->get_bytes_transferred() != _buf_len) {
-              abort(XferFault::BUS_FAULT);
-            }
-            else {
-              markComplete();
-            }
-            break;
-          case BusOpcode::TX_CMD:
-            if (adptr->get_bytes_transferred() > 1) {
-              abort(XferFault::DEV_NOT_FOUND);
-            }
-            else {
-              markComplete();
-            }
-            break;
-          default:
-            abort(XferFault::ILLEGAL_STATE);
-            break;
-        }
-      }
+    case I2CError::master_not_ready:       // Caller failed to wait for one transaction to finish before starting the next
+    case I2CError::arbitration_lost:       // Another master interrupted
+      abort(XferFault::BUS_BUSY);
       break;
-    case XferState::STOP:
-      markComplete();
+    case I2CError::buffer_overflow:        // Not enough room in receive buffer to hold all the data. Bytes dropped.
+    case I2CError::buffer_underflow:       // Not enough data in transmit buffer to satisfy reader. Padding sent.
+    case I2CError::invalid_request:        // Caller asked Master to read more than 256 bytes in one go
+      abort(XferFault::DMA_FAULT);
       break;
-    case XferState::COMPLETE:
-    case XferState::FAULT:
+    case I2CError::master_pin_low_timeout: // SCL or SDA held low for too long. Can be caused by a stuck slave.
+      abort(XferFault::TIMEOUT);
+      break;
+    case I2CError::master_fifo_error:      // Master attempted to send or receive without a START. Programming error.
+    case I2CError::master_fifos_not_empty: // Programming error. FIFOs not empty at start of transaction.
+      abort(XferFault::ILLEGAL_STATE);
+      break;
+    case I2CError::address_nak:
+      abort(XferFault::DEV_NOT_FOUND);
+      break;
+    case I2CError::data_nak:
+      abort(XferFault::DEV_FAULT);
+      break;
+    case I2CError::bit_error:
+      abort(XferFault::UNDEFD_REGISTER);
+      break;
     default:
+      abort(XferFault::NO_REASON);
       break;
   }
   return getFault();
