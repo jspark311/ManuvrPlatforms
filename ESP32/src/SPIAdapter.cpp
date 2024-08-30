aac9b7910cfa75e4db58bc55f2366656b6980f79
Ian Lindsay <ian@joshianlindsay.com>
2024-08-30 01:34:44 -0600

    Better SPI treatment on ESP-IDF 5.3.

--- ESP32/src/SPIAdapter.cpp
+++ ESP32/src/SPIAdapter.cpp
@@ -40,14 +40,18 @@ extern "C" {
 *******************************************************************************/
 
 //static lldesc_t _ll_tx;
-static SPIBusOp* _threaded_op[2]  = {nullptr, nullptr};
-spi_device_handle_t spi_handle[2];
-TaskHandle_t static_spi_thread_id = 0;
+static SPIBusOp* _threaded_op[SPI_HOST_MAX]  = {0};
+static spi_transaction_ext_t txns[SPI_HOST_MAX];
+static spi_device_handle_t spi_handle[SPI_HOST_MAX];
+static TaskHandle_t static_spi_thread_id = 0;
 
 
 static void* IRAM_ATTR spi_worker_thread(void* arg) {
   SPIAdapter* BUSPTR = (SPIAdapter*) arg;
   uint8_t anum = BUSPTR->adapterNumber();
+  _threaded_op[anum] = nullptr;
+  while (!BUSPTR->busOnline()) {  platform.yieldThread();  }
+
   while (1) {
     if (nullptr != _threaded_op[anum]) {
       SPIBusOp* op = _threaded_op[anum];
@@ -85,47 +89,39 @@ static void* IRAM_ATTR spi_worker_thread(void* arg) {
 
 int8_t SPIAdapter::_bus_init() {
 	spi_bus_config_t bus_config;
+  memset(&bus_config, 0xFF, sizeof(spi_bus_config_t));
 	uint32_t b_flags = 0;
 	spi_host_device_t host_id;
-	uint8_t dma_chan;
+	uint8_t dma_chan = SPI_DMA_CH_AUTO;
 	uint8_t anum = adapterNumber();
+  esp_intr_cpu_affinity_t affinity;
 	switch (anum) {
-		case 0:
-			host_id = HSPI_HOST;
-			dma_chan = 2;
-			break;
-		case 1:
-			host_id = VSPI_HOST;
-			dma_chan = 1;
-			break;
-		default:
-			return -2;
+		case 2:			host_id = HSPI_HOST;			break;
+		case 3:			host_id = VSPI_HOST;			break;
+		default:    return -2;
 	}
+  memset(&txns[anum], 0, sizeof(spi_transaction_ext_t));
+  switch (esp_cpu_get_core_id()) {
+    case 0:    affinity = ESP_INTR_CPU_AFFINITY_0;     break;
+    case 1:    affinity = ESP_INTR_CPU_AFFINITY_1;     break;
+    default:   affinity = ESP_INTR_CPU_AFFINITY_AUTO;  break;
+  }
+  _threaded_op[anum] = nullptr;
 
   if (255 != _CLK_PIN) {
     b_flags |= SPICOMMON_BUSFLAG_SCLK;
-    bus_config.sclk_io_num = (gpio_num_t) _CLK_PIN;
-  }
-  else {
-    bus_config.sclk_io_num = GPIO_NUM_NC;
+    bus_config.sclk_io_num = _CLK_PIN;
   }
   if (255 != _MOSI_PIN) {
     b_flags |= SPICOMMON_BUSFLAG_MOSI;
-    bus_config.mosi_io_num = (gpio_num_t) _MOSI_PIN;
-  }
-  else {
-    bus_config.mosi_io_num = GPIO_NUM_NC;
+    bus_config.mosi_io_num = _MOSI_PIN;
   }
   if (255 != _MISO_PIN) {
     b_flags |= SPICOMMON_BUSFLAG_MISO;
-    bus_config.miso_io_num = (gpio_num_t) _MISO_PIN;
+    bus_config.miso_io_num = _MISO_PIN;
   }
-  else {
-    bus_config.miso_io_num = GPIO_NUM_NC;
-  }
-	bus_config.quadwp_io_num   = GPIO_NUM_NC;    // Not used
-	bus_config.quadhd_io_num   = GPIO_NUM_NC;    // Not used
 	bus_config.max_transfer_sz = 65535;   // 0 means use default.
+  bus_config.isr_cpu_id      = affinity;
   bus_config.flags           = b_flags;
   bus_config.intr_flags      = ESP_INTR_FLAG_IRAM;
 
@@ -151,7 +147,7 @@ int8_t SPIAdapter::_bus_init() {
   dev_config.clock_source     = SPI_CLK_SRC_DEFAULT;
 	dev_config.clock_speed_hz   = 16000000;
 	dev_config.spics_io_num     = (gpio_num_t) -1;
-	dev_config.flags            = SPI_DEVICE_NO_DUMMY;
+	dev_config.flags            = (SPI_DEVICE_NO_DUMMY | SPI_DEVICE_HALFDUPLEX);
 	dev_config.queue_size       = 1;
 	dev_config.pre_cb           = nullptr;
 	dev_config.post_cb          = nullptr;
@@ -165,9 +161,9 @@ int8_t SPIAdapter::_bus_init() {
   unsigned long _thread_id = 0;
   PlatformThreadOpts topts;
   topts.thread_name = (char*) "SPI";
-  topts.stack_sz    = 2048;
+  topts.stack_sz    = 4096;
   topts.priority    = 0;
-  topts.core        = 1;   // TODO: Is this the best choice? Might use a preprocessor define.
+  //topts.core        = 1;   // TODO: Is this the best choice? Might use a preprocessor define.
 
 	platform.createThread(&_thread_id, nullptr, spi_worker_thread, (void*) this, &topts);
   static_spi_thread_id = (TaskHandle_t) _thread_id;
@@ -197,8 +193,8 @@ XferFault SPIBusOp::begin() {
 	uint8_t anum = _bus->adapterNumber();
   //time_began    = micros();
   switch (anum) {
-    case 0:
-    case 1:
+    case 2:
+    case 3:
 			if (nullptr == _threaded_op[anum]) {
         if ((nullptr == callback) || (0 == callback->io_op_callahead(this))) {
           _threaded_op[anum] = this;
@@ -216,7 +212,7 @@ XferFault SPIBusOp::begin() {
         ret = XferFault::BUS_BUSY;
 			}
       break;
-    case 2:   // SPI workflow
+
     default:
       ret = XferFault::BUS_FAULT;
       abort(ret);
@@ -226,6 +222,7 @@ XferFault SPIBusOp::begin() {
 }
 
 
+
 /**
 * Called from the ISR to advance this operation on the bus.
 * Stay brief. We are in an ISR.
@@ -233,34 +230,44 @@ XferFault SPIBusOp::begin() {
 * @return 0 on success. Non-zero on failure.
 */
 int8_t SPIBusOp::advance_operation(uint32_t status_reg, uint8_t data_reg) {
-	static spi_transaction_t txns[2];
 	XferFault ret = XferFault::BUS_FAULT;
   bool run_xfer = false;
 	set_state(XferState::INITIATE);  // Indicate that we now have bus control.
-	uint8_t anum = _bus->adapterNumber();
-	memset(&txns[anum], 0, sizeof(spi_transaction_t));
-  txns[anum].flags |= SPI_DEVICE_HALFDUPLEX;
+	const uint8_t anum = _bus->adapterNumber();
 
-  if (_param_len) {
-    set_state(XferState::ADDR);
-    memcpy(&(txns[anum].tx_data), xfer_params, transferParamLength());
-		txns[anum].flags |= SPI_TRANS_USE_TXDATA;
-    txns[anum].length = _param_len * 8;
-    run_xfer = true;
+  spi_device_acquire_bus(spi_handle[anum], portMAX_DELAY);
+  memset(&txns[anum], 0, sizeof(spi_transaction_ext_t));
+
+  uint64_t addr_tmp = 0;
+  switch (_param_len) {
+    case 8:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[7];
+    case 7:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[6];
+    case 6:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[5];
+    case 5:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[4];
+    case 4:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[3];
+    case 3:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[2];
+    case 2:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[1];
+    case 1:  addr_tmp = (addr_tmp << 8);  addr_tmp = xfer_params[0];
+      set_state(XferState::ADDR);
+      txns[anum].address_bits = (_param_len << 3);
+      txns[anum].base.addr    = addr_tmp;
+      txns[anum].base.flags |= SPI_TRANS_VARIABLE_ADDR;
+    default:
+      break;
   }
 
   if (_buf_len) {
     switch (get_opcode()) {
       case BusOpcode::TX:
         set_state(XferState::TX_WAIT);
-				txns[anum].tx_buffer = _buf;
-				txns[anum].length = _buf_len * 8;
+				txns[anum].base.tx_buffer = _buf;
+				txns[anum].base.length = _buf_len * 8;
         run_xfer = true;
         break;
       case BusOpcode::RX:
         set_state(XferState::RX_WAIT);
-				txns[anum].rx_buffer = _buf;
-				txns[anum].rxlength = _buf_len * 8;
+				txns[anum].base.rx_buffer = _buf;
+				txns[anum].base.rxlength = _buf_len * 8;
         run_xfer = true;
         break;
       default:
@@ -271,7 +278,7 @@ int8_t SPIBusOp::advance_operation(uint32_t status_reg, uint8_t data_reg) {
 
   if (run_xfer) {
     _assert_cs(true);
-    if (ESP_OK == spi_device_queue_trans(spi_handle[anum], &txns[anum], portMAX_DELAY)) {
+    if (ESP_OK == spi_device_queue_trans(spi_handle[anum], &txns[anum].base, portMAX_DELAY)) {
       spi_transaction_t* pending_txn;
       if (ESP_OK == spi_device_get_trans_result(spi_handle[anum], &pending_txn, portMAX_DELAY)) {
         ret = XferFault::NONE;
@@ -292,6 +299,7 @@ int8_t SPIBusOp::advance_operation(uint32_t status_reg, uint8_t data_reg) {
   else {
     markComplete();
   }
+  spi_device_release_bus(spi_handle[anum]);
   return 0;
 }
 
