d800d6f490a185c9bddbd55e6b7145abcbe8b391
Ian Lindsay <ian@joshianlindsay.com>
2025-10-13 16:01:07 -0600

    CI fix. I hope.

--- Linux/example/ImageAPI/downloadDeps.sh
+++ Linux/example/ImageAPI/downloadDeps.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# This script is meant to go fetch the most recent versions of various libraries that
+#   this program has been written against.
+mkdir -p lib
+
+# CppPotpourri
+rm -rf lib/CppPotpourri
+git clone https://github.com/jspark311/CppPotpourri.git lib/CppPotpourri
