cb2dfc3d9cad0083ec59649bb2be4dbfadcd9eb6
Ian Lindsay <ian@joshianlindsay.com>
2025-12-29 23:13:05 -0700

    Changes to ESP32 UART driver to facilitate re-init.

--- ESP32/src/UART.cpp
+++ ESP32/src/UART.cpp
@@ -36,18 +36,19 @@ extern "C" {
 
 
 static const char* TAG = "uart_drvr";
+TaskHandle_t uart_polling_task_handle;
 QueueHandle_t uart_queues[]  = {nullptr, nullptr, nullptr};
 UARTAdapter* uart_instances[] = {nullptr, nullptr, nullptr};
 
 
-
-static void uart_event_task(void *pvParameters) {
+static void IRAM_ATTR uart_event_task(void *pvParameters) {
   for (;;) {
     for (uint8_t port_idx = 0; port_idx < 3; port_idx++) {
-      if (nullptr != uart_queues[port_idx]) {
+      if (nullptr != uart_instances[port_idx]) {
         uart_instances[port_idx]->irq_handler();
       }
     }
+    platform.yieldThread();
   }
   vTaskDelete(NULL);
 }
@@ -62,14 +63,18 @@ static void uart_event_task(void *pvParameters) {
 */
 void PlatformUART::irq_handler() {
   uart_event_t event;
+  bzero((void*) &event, sizeof(uart_event_t));
   // Waiting for UART event.
-  if (xQueueReceive(uart_queues[ADAPTER_NUM], (void * )&event, (TickType_t)portMAX_DELAY)) {
+  if (nullptr == uart_queues[ADAPTER_NUM]) {  return;  }
+  if (xQueueReceive(uart_queues[ADAPTER_NUM], (void *)&event, (TickType_t)10)) {
+    //c3p_log(LOG_LEV_INFO, __PRETTY_FUNCTION__, "UART IRQ");
     switch (event.type) {
       //Event of UART receving data
       case UART_DATA:
         {
           size_t dlen = 0;
           if (ESP_OK == uart_get_buffered_data_len((uart_port_t) ADAPTER_NUM, &dlen)) {
+            //c3p_log(LOG_LEV_INFO, __PRETTY_FUNCTION__, "\t RX data (%d bytes)", dlen);
             if (dlen > 0) {
               last_byte_rx_time = millis();
               uint8_t dtmp[dlen + 4];
@@ -162,11 +167,11 @@ int8_t PlatformUART::_pf_poll() {
       uint8_t side_buffer[TX_COUNT] = {0, };
       const int32_t PEEK_COUNT = _tx_buffer.peek(side_buffer, TX_COUNT);
       const int32_t BYTES_WRITTEN = uart_write_bytes((uart_port_t) ADAPTER_NUM, (const char*) side_buffer, (size_t) PEEK_COUNT);
-      _flushed = _tx_buffer.isEmpty();
       if (BYTES_WRITTEN > 0) {
         _tx_buffer.cull(BYTES_WRITTEN);
         return_value |= 1;
       }
+      uart_wait_tx_idle_polling((uart_port_t) ADAPTER_NUM);
     }
   }
   if (rxCapable()) {
@@ -174,99 +179,115 @@ int8_t PlatformUART::_pf_poll() {
       return_value |= 1;
     }
   }
+  _flushed = _tx_buffer.isEmpty();
   return return_value;
 }
 
 
 int8_t PlatformUART::_pf_init() {
+  int8_t ret = -1;
   if (ADAPTER_NUM < 3) {
     if (nullptr == uart_queues[ADAPTER_NUM]) {
-      uart_word_length_t     databits;
-      uart_parity_t          par_bits;
-      uart_stop_bits_t       stp_bits;
-      uart_hw_flowcontrol_t  flowctrl;
       bool spawn_thread = !((nullptr != uart_queues[0]) || (nullptr != uart_queues[1]) || (nullptr != uart_queues[2]));
-
-      switch (_opts.stop_bits) {
-        default:
-        case UARTStopBit::STOP_1:    stp_bits = UART_STOP_BITS_1;     break;
-        case UARTStopBit::STOP_1_5:  stp_bits = UART_STOP_BITS_1_5;   break;
-        case UARTStopBit::STOP_2:    stp_bits = UART_STOP_BITS_2;     break;
-      }
-      switch (_opts.parity) {
-        default:
-        case UARTParityBit::NONE:    par_bits = UART_PARITY_DISABLE;  break;
-        case UARTParityBit::EVEN:    par_bits = UART_PARITY_EVEN;     break;
-        case UARTParityBit::ODD:     par_bits = UART_PARITY_ODD;      break;
-      }
-      switch (_opts.flow_control) {
-        default:                           // These are software ideas. Hardware sees no flowcontrol.
-        case UARTFlowControl::XON_XOFF_R:  // These are software ideas. Hardware sees no flowcontrol.
-        case UARTFlowControl::XON_XOFF_T:  // These are software ideas. Hardware sees no flowcontrol.
-        case UARTFlowControl::XON_XOFF_RT: // These are software ideas. Hardware sees no flowcontrol.
-        case UARTFlowControl::NONE:         flowctrl = UART_HW_FLOWCTRL_DISABLE;  break;
-        case UARTFlowControl::RTS:          flowctrl = UART_HW_FLOWCTRL_RTS;      break;
-        case UARTFlowControl::CTS:          flowctrl = UART_HW_FLOWCTRL_CTS;      break;
-        case UARTFlowControl::RTS_CTS:      flowctrl = UART_HW_FLOWCTRL_CTS_RTS;  break;
-      }
-      switch (_opts.bit_per_word) {
-        default:
-        case 8:  databits = UART_DATA_8_BITS;  break;
-        case 7:  databits = UART_DATA_7_BITS;  break;
-        case 6:  databits = UART_DATA_6_BITS;  break;
-        case 5:  databits = UART_DATA_5_BITS;  break;
-      }
-      uart_config_t uart_config = {
-        .baud_rate = (int) _opts.bitrate,
-        .data_bits = databits,
-        .parity = par_bits,
-        .stop_bits = stp_bits,
-        .flow_ctrl = flowctrl,
-        .rx_flow_ctrl_thresh = 122,  // The FIFO is 128 bytes (I think?)
-        .source_clk = UART_SCLK_DEFAULT
-      };
-      if (ESP_OK == uart_param_config((uart_port_t) ADAPTER_NUM, &uart_config)) {
-        unsigned int rxpin  = (255 == _RXD_PIN)  ? UART_PIN_NO_CHANGE : _RXD_PIN;
-        unsigned int txpin  = (255 == _TXD_PIN)  ? UART_PIN_NO_CHANGE : _TXD_PIN;
-        unsigned int rtspin = (255 == _RTS_PIN) ? UART_PIN_NO_CHANGE : _RTS_PIN;
-        unsigned int ctspin = (255 == _CTS_PIN) ? UART_PIN_NO_CHANGE : _CTS_PIN;
-        if (ESP_OK == uart_set_pin((uart_port_t) ADAPTER_NUM, txpin, rxpin, rtspin, ctspin)) {
-          _adapter_set_flag(UART_FLAG_HAS_TX, (UART_PIN_NO_CHANGE != txpin));
-          _adapter_set_flag(UART_FLAG_HAS_RX, (UART_PIN_NO_CHANGE != rxpin));
-
-          uart_queues[ADAPTER_NUM] = (QueueHandle_t) malloc(sizeof(QueueHandle_t));
-          if (nullptr != uart_queues[ADAPTER_NUM]) {
-            uart_instances[ADAPTER_NUM] = this;
-            const uint32_t rx_ring_size = rxCapable() ? UART_FIFO_CAPACITY : 4;
-            const uint32_t tx_ring_size = txCapable() ? UART_FIFO_CAPACITY : 4;
-            if (ESP_OK == uart_driver_install((uart_port_t) ADAPTER_NUM, rx_ring_size, tx_ring_size, 10, &uart_queues[ADAPTER_NUM], 0)) {
-              const uart_intr_config_t intr_conf {
-                // UART_FRM_ERR_INT: Triggered when the receiver detects a data frame error .
-                // UART_PARITY_ERR_INT: Triggered when the receiver detects a parity error in the data.
-                // rx_timeout_thresh must be greater than rxfifo_full_thresh if UART_INTR_RXFIFO_TOUT is enabled.
-                .intr_enable_mask = (UART_INTR_TXFIFO_EMPTY | UART_INTR_RXFIFO_FULL | UART_INTR_RXFIFO_TOUT | UART_INTR_RXFIFO_OVF | UART_INTR_TX_DONE),
-                .rx_timeout_thresh        = 20,
-                .txfifo_empty_intr_thresh = 16,
-                .rxfifo_full_thresh       = 16
-              };
-              if (ESP_OK == uart_intr_config((uart_port_t) ADAPTER_NUM, &intr_conf)) {
-                _adapter_set_flag(UART_FLAG_UART_READY);
-                _adapter_clear_flag(UART_FLAG_PENDING_CONF | UART_FLAG_PENDING_RESET);
-                _flushed = true;
-                if (spawn_thread) {
-                  xTaskCreate(uart_event_task, "uart_task", 3000, NULL, 12, NULL);
-                }
-                uart_enable_tx_intr((uart_port_t) ADAPTER_NUM, 1, 16);
-                uart_enable_rx_intr((uart_port_t) ADAPTER_NUM);
-                return 0;
-              }
+      uart_instances[ADAPTER_NUM] = this;
+      const uint32_t rx_ring_size = (255 != _RXD_PIN) ? UART_FIFO_CAPACITY : 4;
+      const uint32_t tx_ring_size = (255 != _TXD_PIN) ? UART_FIFO_CAPACITY : 4;
+      if (ESP_OK == uart_driver_install((uart_port_t) ADAPTER_NUM, rx_ring_size, tx_ring_size, 10, &uart_queues[ADAPTER_NUM], 0)) {
+        //ESP_LOGI(TAG, "uart_driver_install: %p", uart_queues[ADAPTER_NUM]);
+        ret--;
+        const uart_intr_config_t intr_conf {
+          // UART_FRM_ERR_INT: Triggered when the receiver detects a data frame error .
+          // UART_PARITY_ERR_INT: Triggered when the receiver detects a parity error in the data.
+          // rx_timeout_thresh must be greater than rxfifo_full_thresh if UART_INTR_RXFIFO_TOUT is enabled.
+          .intr_enable_mask = (UART_INTR_TXFIFO_EMPTY | UART_INTR_RXFIFO_FULL | UART_INTR_RXFIFO_TOUT | UART_INTR_RXFIFO_OVF | UART_INTR_TX_DONE),
+          .rx_timeout_thresh        = 20,
+          .txfifo_empty_intr_thresh = 16,
+          .rxfifo_full_thresh       = 16
+        };
+        if (ESP_OK == uart_intr_config((uart_port_t) ADAPTER_NUM, &intr_conf)) {
+          _adapter_set_flag(UART_FLAG_UART_READY);
+          _adapter_clear_flag(UART_FLAG_PENDING_CONF | UART_FLAG_PENDING_RESET);
+          _flushed = true;
+          ret--;
+          if (spawn_thread) {
+            xTaskCreate(uart_event_task, "uart_task", 3000, NULL, 12, &uart_polling_task_handle);
+          }
+          if ((255 == _TXD_PIN) || (ESP_OK == uart_enable_tx_intr((uart_port_t) ADAPTER_NUM, 1, 16))) {
+            ret--;
+            if ((255 == _RXD_PIN) || (ESP_OK == uart_enable_rx_intr((uart_port_t) ADAPTER_NUM))) {
+              ret = 0;
             }
           }
         }
       }
     }
+    else {
+      ret = 0;
+    }
   }
-  return -1;
+
+  //
+  if (0 == ret) {
+    ret = -10;
+    uart_word_length_t     databits;
+    uart_parity_t          par_bits;
+    uart_stop_bits_t       stp_bits;
+    uart_hw_flowcontrol_t  flowctrl;
+
+    switch (_opts.stop_bits) {
+      default:
+      case UARTStopBit::STOP_1:    stp_bits = UART_STOP_BITS_1;     break;
+      case UARTStopBit::STOP_1_5:  stp_bits = UART_STOP_BITS_1_5;   break;
+      case UARTStopBit::STOP_2:    stp_bits = UART_STOP_BITS_2;     break;
+    }
+    switch (_opts.parity) {
+      default:
+      case UARTParityBit::NONE:    par_bits = UART_PARITY_DISABLE;  break;
+      case UARTParityBit::EVEN:    par_bits = UART_PARITY_EVEN;     break;
+      case UARTParityBit::ODD:     par_bits = UART_PARITY_ODD;      break;
+    }
+    switch (_opts.flow_control) {
+      default:                           // These are software ideas. Hardware sees no flowcontrol.
+      case UARTFlowControl::XON_XOFF_R:  // These are software ideas. Hardware sees no flowcontrol.
+      case UARTFlowControl::XON_XOFF_T:  // These are software ideas. Hardware sees no flowcontrol.
+      case UARTFlowControl::XON_XOFF_RT: // These are software ideas. Hardware sees no flowcontrol.
+      case UARTFlowControl::NONE:         flowctrl = UART_HW_FLOWCTRL_DISABLE;  break;
+      case UARTFlowControl::RTS:          flowctrl = UART_HW_FLOWCTRL_RTS;      break;
+      case UARTFlowControl::CTS:          flowctrl = UART_HW_FLOWCTRL_CTS;      break;
+      case UARTFlowControl::RTS_CTS:      flowctrl = UART_HW_FLOWCTRL_CTS_RTS;  break;
+    }
+    switch (_opts.bit_per_word) {
+      default:
+      case 8:  databits = UART_DATA_8_BITS;  break;
+      case 7:  databits = UART_DATA_7_BITS;  break;
+      case 6:  databits = UART_DATA_6_BITS;  break;
+      case 5:  databits = UART_DATA_5_BITS;  break;
+    }
+    uart_config_t uart_config = {
+      .baud_rate = (int) _opts.bitrate,
+      .data_bits = databits,
+      .parity = par_bits,
+      .stop_bits = stp_bits,
+      .flow_ctrl = flowctrl,
+      .rx_flow_ctrl_thresh = 122,  // The FIFO is 128 bytes (I think?)
+      .source_clk = UART_SCLK_DEFAULT
+    };
+    if (ESP_OK == uart_param_config((uart_port_t) ADAPTER_NUM, &uart_config)) {
+      int rxpin  = (255 == _RXD_PIN) ? -1 : _RXD_PIN;
+      int txpin  = (255 == _TXD_PIN) ? -1 : _TXD_PIN;
+      int rtspin = (255 == _RTS_PIN) ? -1 : _RTS_PIN;
+      int ctspin = (255 == _CTS_PIN) ? -1 : _CTS_PIN;
+      ret--;
+      if (ESP_OK == uart_set_pin((uart_port_t) ADAPTER_NUM, txpin, rxpin, rtspin, ctspin)) {
+        _adapter_set_flag(UART_FLAG_HAS_TX, (-1 != txpin));
+        _adapter_set_flag(UART_FLAG_HAS_RX, (-1 != rxpin));
+        uart_flush_input((uart_port_t) ADAPTER_NUM);
+        //xQueueReset(uart_queues[ADAPTER_NUM]);
+        ret = 0;
+      }
+    }
+  }
+  return ret;
 }
 
 
